12f2a079256c75f9442bf8daa184e3713b7e5e98
Titus <72074715+Titus777@users.noreply.github.com>
2023-04-20 00:26:47 +0300

    updated user-status

--- pages/api/update/user-status/[...email].js
+++ pages/api/update/user-status/[...email].js
@@ -11,15 +11,14 @@ export default async function handler(req,res){
     }
     try{
         const {email} = req.query
-        const response = await User.findOneAndUpdate({email: email[0]},{new:false})
-        if(response){
+        const response = await User.findOneAndUpdate({email: email[0]},{new:false},{rawResults:true})
+        if(response.modifiedCount == 1){
             res.status(200).json(response)
             return
         }else{
             console.log("here")
             res.status(404).send("Cannot find your details, please refresh")
         }
-
     }catch(e){
         console.error(e)
         res.status(500).send("Server error")
