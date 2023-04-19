359c73ab67bfc3e50618f6929e8ec24fc59debaf
Titus777 <4mahet92@solent.ac.uk>
2023-04-19 21:21:33 +0100

    login fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,10 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import Auth0Provider from "next-auth/providers/auth0"
 import CredentialsProvider from "next-auth/providers/credentials";
-import dbConnect from "../../../lib/dbConnect";
-import User from "../../../common/model/User";
-import {hash,compare} from "bcrypt"
-
+import axios from "axios"
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
 
@@ -40,106 +37,29 @@ export const authOptions= {
 
       async authorize(credentials,req){
         console.log("I started")
-        await dbConnect();
+        let response = await fetch(`${process.env.NEXTAUTH_URL}/api/register`, {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: {"Content-Type":"application/json"}
+        })
 
-        async function register(){
-          try{
-              if(!credentials){
-      
-                  return null
-              }
-          
-              const {email, password} = credentials
-          
-                  hash(password, 10, async function(err,hash){
-          
-                      const existingUser = await User.findOne({email})
-                      if(existingUser){
-                          return null
-                      }
-                      
-                      const user = new User({
-                          email: email,
-                          password: hash,
-                          
-                          details: [
-                              {first_name: "",
-                               last_name: "",
-                               activity: "",
-                               diet: "",
-                               exercise_time: 0,
-                               favorite_food: "",
-                               favorite_recipes:[ {} ] ,
-                               uploaded_recipes:[ {} ] ,
-                               goals: [""],
-                               sex: "",
-                               pref_intake:0,
-                               weight:0,
-                               height:0 
-                              }
-                          ],
-                          notes: [
-                              {dates: [],
-                              notes: [""],
-                              recipe_notes: [{}]}
-                              ],
-                          new: true,
-      
-                          created_at: new Date()
-                      })
-                      await user.save().catch(error => 
-                          {
-                              return null
-                              console.error(error)
-                          })
-                      if(!err){
-                          return user
-                          
-                      }else{
-                          return null
-                      }            
-                  })
-          }catch(e){
-              console.error(e)
-              return null
-          }
-         
+        let user = await response.json()
+
+        if(response.status == 422){
+          response = await fetch(`${process.env.NEXTAUTH_URL}/api/login`,{
+            method:"POST",
+            body: JSON.stringify(credentials),
+            headers: {"Content-Type":"application/json"}
+          })
+          user = await response.json()
         }
-        const registered = await register()
-        if(registered == null){
-          try{
-            if(!credentials){
-                
-                return null
-            }
-        
-            const {email, password} = credentials
-                hash(password, 10, async function(err,hash){
-         
-                    
-                    const user = await User.findOne({email: email})
-    
-                    if(!user){
-                      
-                        return null;
-                    }
-                
-                    const isMatch = await compare(password, user.password);
-            
-                    if (!isMatch) {
-                      console.error("Is not match")
-                      return null;
-                    }
-                    
-                    return user
-                })
-       
-           
-        }catch(e){
-            return null
+        if(response.ok && user){
+          console.log("I will return user")
+          return user
+        }else{
+          return null
         }
-       
-        }}
+      }
     }),
     
     GoogleProvider({
